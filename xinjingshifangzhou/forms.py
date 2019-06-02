b4ccfaecb1fa9cdd75f7879f1de35972f77cb75a
whatyouknow123 <1211183540@qq.com>
2019-06-02 23:33:08 +0800

    change form data

--- xinjingshifangzhou/forms.py
+++ xinjingshifangzhou/forms.py
@@ -5,23 +5,21 @@ from models import ApplicationForm, LeaveNote
 
 
 class TestApplicationForm(forms.Form):
-    school = forms.CharField()
+    type = forms.CharField()
     name = forms.CharField(max_length=100,min_length=3,error_messages={'required':'用户名不能为空','min_length':'最少不能少于3个字符','max_length':'最多不能超过100个字符'})
-    email = forms.EmailField(error_messages={'invalid':'请输入正确的邮箱!','required':'邮箱不能为空'})
+    #email = forms.EmailField(error_messages={'invalid':'请输入正确的邮箱!','required':'邮箱不能为空'})
     phone = forms.CharField(validators=[validators.RegexValidator(r'1[345678]\d{9}',message='请输入正确的手机号码')])
-    address = forms.CharField(error_messages={'invalid':'请输入正确的地址!','required':'地址不能为空'})
+    #address = forms.CharField(error_messages={'invalid':'请输入正确的地址!','required':'地址不能为空'})
     age = forms.IntegerField(error_messages={'invalid':'请输入正确的年龄!','required':'年龄不能为空'})
-    height = forms.FloatField(error_messages={'invalid':'请输入正确的身高!','required':'身高不能为空'})
-
+    #height = forms.FloatField(error_messages={'invalid':'请输入正确的身高!','required':'身高不能为空'})
+    gender = forms.CharField()
 
 def UpdateApplication(result):
     ApplicationForm.objects.create(phone=result["phone"],
-                                        school=result['school'],
-                                        email=result['email'],
+                                        type=result['type'],
                                         name=result['name'],
-                                        address=result['address'],
                                         age=result['age'],
-                                        height=result['height'])
+                                        gender=result['gender'])
 
 
 
